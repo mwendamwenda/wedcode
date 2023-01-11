f0638c2a706929563b094cf53453da06d8485651
mwendamwenda <benmwenda83@gmail.com>
2023-01-10 19:41:33 -0800

    coersion and conversion

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('i have nothing agsinst you');
+//type conversion
+const inputYear = '1992';
+console.log(Number(inputYear));
+console.log(Number(inputYear) + 18);//we have to convert the string '1992' to a number using an inbuilt function;
+
+console.log(Number('Benson'));
+console.log(String(23));
+
+// type coersion
+console.log('i am ' + 23 + 'years old');
+console.log('23' - '10'- 3);
+console.log('23'* '2');
+
+let n = '1' + 1 //this will be 11
+n = n -1;
+console.log(n);//thi will be 10
