08f330eb230fb167f1e0230898bd6f2c01b8b296
mwendamwenda <benmwenda83@gmail.com>
2023-01-10 21:05:33 -0800

    equality operators

--- index.js
+++ index.js
@@ -37,3 +37,25 @@ if(height){
 }else{
     console.log('height is not defined');
 }
+
+const age = 18;
+if(age === 18) console.log('you just became and adult(strict)'); ///if we have one line to be excuted we dont need to use curly braces in if else statement
+
+// the === does not perfome type coersion
+if(age === 18) console.log('you just became and adult(loose)');
+
+
+
+const favourite = Number(prompt("what's your favourite number"));
+console.log(favourite);
+console.log(typeof favourite);
+
+if(favourite === 23){
+    console.log('cool! 23 is an amzing number');
+}else if(favourite === 7){
+    console.log('7 is also a cool number');
+}else{
+    console.log('the number is not 23 or 7 so its not cool');
+}
+
+if (favourite !== 23) console.log('why not 23');
