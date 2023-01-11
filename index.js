a85d9365606ada4631f96a2a06afcec7316d0fd1
mwendamwenda <benmwenda83@gmail.com>
2023-01-10 20:00:50 -0800

    truthy and falsey values

--- index.js
+++ index.js
@@ -14,3 +14,26 @@ console.log('23'* '2');
 let n = '1' + 1 //this will be 11
 n = n -1;
 console.log(n);//thi will be 10
+
+
+// 5 falsey values 0, "" ,undefined ,null and NaN;
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('mwenda'));
+console.log(Boolean(''));
+console.log(Boolean({}));
+
+const money = 0;
+ 
+if(money){
+    console.log("don't spend it all");
+}else{
+    console.log('you should get a job');
+}
+
+let height;
+if(height){
+    console.log('YAY! height is defined');
+}else{
+    console.log('height is not defined');
+}
